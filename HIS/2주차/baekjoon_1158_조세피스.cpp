6ea9f198645b5c2a0af0c90c90b35a0e5c33dd39
InSoo <his9989@naver.com>
2019-10-04 21:53:17 +0900

    Add files via upload

---
+++ HIS/2주차/baekjoon_1158_조세피스.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<queue>
+using namespace std;
+
+queue<int> que;
+queue<int> que2;
+
+int main() {
+	int N, M;
+	int first = 0;
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++)
+		que.push(i);
+	while (que.empty() != true) {
+		for (int j = 0; j < M - 1; j++) {
+			first = que.front();
+			que.pop();
+			que.push(first);
+		}
+		que2.push(que.front());
+		que.pop();
+	}
+	int size = que2.size();
+	cout << "<";
+	for (int i = 0; i < size; i++) {
+		cout << que2.front();
+		if (i != size - 1) cout << ", ";
+		que2.pop();
+	}
+	cout << ">";
+}
